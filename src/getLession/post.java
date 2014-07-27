9828af8f90bc3b6f75ea35f42edfd527a2507b8d
ian-cuc <yinyien.ion@gmail.com>
2014-07-27 10:45:16 +0800

    Update post.java

--- src/getLession/post.java
+++ src/getLession/post.java
@@ -31,14 +31,14 @@ public  class post
    public  String viewPage(String Cookie) throws IOException  
    {  
        StringBuilder sbR = new StringBuilder();  
-       //闂佺懓鐏氶幐鍝ユ閹鳖槃L闁哄鏅濋崑鐐垫暜閿燂拷
+       
        URL url1 = new URL("http://jw.cuc.edu.cn/academic/student/currcourse/currcourse.jsdo?groupId=&moduleId=2000");  
        HttpURLConnection connection1 = (HttpURLConnection) url1.openConnection();  
-       //缂傚倷鐒﹂悷锕�耿閸ヮ剙绀夐柍銉ㄦ珪閻濄倝姊洪陇鍏屾繛鍛劥閵囨劙寮撮悙鏉戯拷闂佹眹鍔岄崹绫祇kie  
+       
        connection1.setRequestProperty("Cookie", Cookie);  
-       //闁荤姴娲╅褑銇愰崶銊︿氦闁哄倹瀵х粈锟芥煟閵娿儱顏柕鍡楊檨濡啴路瀹ュ嫮绠氶梺璇″弾閸ㄤ即宕虹�鎭�  
+      
        BufferedReader br1 = new BufferedReader(new InputStreamReader(connection1.getInputStream()));  
-       //闂佸憡鐟﹂悧鐐垫崲閹达箑鐐婇柣鎰仛閻ｅ崬顪㈤妶鍥ㄦ毈婵炵》鎷穊r1闁哄鍋熼幉姒朢  
+       
        String line1= br1.readLine();  
        while (line1 != null) {  
            sbR.append(line1); 
@@ -99,15 +99,12 @@ public  class post
 //	  String a=post1.getHtmlCode("201206113009","201206113009");
 	  System.out.println(a);
   }
-  //闂佸吋鍎抽崲鑼鏉堚晜瀚氶柟瑙勫姂閿熻棄顪㈤妶鍥ㄦ毈婵為棿鍗抽幆鍐礆閻氱尘l婵炲濯寸徊鍧楁偉濠婂拑鎷锋俊銈呭枤閸庡﹤鈽夐幘璇″殝缂佹唻鎷�  
+  
   public  String getHtmlCode(String  username0 ,String password) throws IOException {    
-      //闁哄鏅濋崑鐐垫暜閹绢喖鎹堕柡澶嬪缁诧拷
+      
       String surl = "http://jw.cuc.edu.cn/academic/j_acegi_security_check"; 
       HttpURLConnection httpurlconnection = null;
-      /**   
-       * 婵☆偓绲鹃悧鏇㈠储濞戞碍鍟哄ù锝呮啞鐎氱澔RL婵炴垶鎸搁鍥р枔閹鳖槃LConnection闁诲海鏁搁、濠囨儊娴犲违闁跨喓绁礡LConnection闂佸憡鐟崹顖涚閹烘埈鍤楅柛顐ｇ箥閸熷洭鏌￠崟顒佸暗婵炲牊鍨剁粋鎺戭吋缁傤洃閻庣數澧楅〃婊堝春瀹�嫬违闁稿本绋撳Σ宄扳攽閳ュ啿浜扮紒鎲嬫嫹// Using   
-       *  java.net.URL and //java.net.URLConnection   
-       */   
+      
       URL url = new URL(surl);  
       httpurlconnection = (HttpURLConnection)url.openConnection();
       httpurlconnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
@@ -116,9 +113,9 @@ public  class post
       httpurlconnection = (HttpURLConnection) url.openConnection();
       httpurlconnection.setFollowRedirects(true);
       httpurlconnection.setInstanceFollowRedirects(true);
-      httpurlconnection.setDoOutput(true); // 闂傚倸娴勯幏鐑解�閸涙潙瑙﹂柟瀵稿仦缁犳盯鏌涢弬鐚存嫹婵炲懏甯″畷妯衡枎閹邦厽顔嶉梺鍦閹凤拷      httpurlconnection.setDoInput(true); // 
+      httpurlconnection.setDoOutput(true); 
       httpurlconnection.setInstanceFollowRedirects(false); 
-      httpurlconnection.setUseCaches(false); // 闂佸吋鍎抽崲鑼闁秴瀚夌�銉ユ噹椤偤鏌涢敐鍐ㄥ婵炴挸鐖煎顒勫箥椤旂晫鏆犳繛锝呮礌閸撴繃瀵奸敓锟�     httpurlconnection.setAllowUserInteraction(false);
+      httpurlconnection.setUseCaches(false); 
       httpurlconnection.setRequestMethod("POST");
       httpurlconnection.addRequestProperty("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
       httpurlconnection.setRequestProperty("Referer","http://jw.cuc.edu.cn/academic/common/security/login.jsp?login_error=1");
